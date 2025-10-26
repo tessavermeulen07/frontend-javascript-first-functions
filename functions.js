764c126ea6cbbca3c245598293527730336cf694
tessavermeulen07 <tessavermeulen@me.com>
2025-10-26 11:40:40 +0100

    functions.js opdrachten klaar

--- functions.js
+++ functions.js
@@ -2,32 +2,63 @@
 // Schrijf een functie die de string "Goedemorgen" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Goedemorgen"
 
-function morningGreeter() {
-  return "Goedemorgen";
-}
-
-const greeting = morningGreeter();
-
-console.log(greeting);
+// function morningGreeter() {
+//   return "Goedemorgen";
+// }
+//
+// const greeting = morningGreeter();
+//
+// console.log(greeting);
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+console.log('Opdracht 1');
 
+function getSchool() {
+    return 'Novi Hogeschool';
+}
+
+const school = getSchool();
+console.log(school);
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+console.log('Opdracht 2');
 
+function getCompliment() {
+    return 'Lekker bezig met die functies, Tessa';
+}
+
+const compliment = getCompliment();
+console.log(compliment);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+console.log('Opdracht 3');
+
+function getGrades() {
+    return [8, 8.5, 6, 7];
+}
+
+const grades = getGrades();
+console.log(grades);
 
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+console.log('Opdracht 4');
+
+function getDetails() {
+    return {firstName: 'Tessa', lastName: 'Vermeulen'};
+}
+
+const details = getDetails();
+console.log(details)
